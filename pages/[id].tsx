a52b5aab3749187e9c2ed6a2aa1a33dc1aff07f8
Lisandro Marchionni <djlm2140@gmail.com>
2023-02-07 16:45:07 -0300

    ported app to next with firestore backend

---
+++ pages/[id].tsx
@@ -0,0 +1,48 @@
+import { db } from "../firebase-config";
+import Head from "next/head";
+
+export default function ({ id }) {
+  return (
+    <>
+      <Head>
+        <title>Page not found</title>
+      </Head>
+      <h2 className="front">404 - {id} Not found</h2>
+      <h2 className="back">404 - {id} Not found</h2>
+      <p>The URL is wrong or misstyped.</p>
+      <a
+        className="badge"
+        href="https://github.com/LisandroMarcho/licho.tk"
+        target="_blank"
+      >
+        <img
+          src="https://img.shields.io/badge/Code-GitHub-%23117a65"
+          alt="Repository badge"
+        />
+      </a>
+    </>
+  );
+}
+
+export async function getServerSideProps({ params }) {
+  const { id } = params;
+  const docSnap = await db.collection("shorts").doc(id).get();
+
+  if (docSnap.exists) {
+    const docData = docSnap.data();
+
+    return {
+      redirect: {
+        permanent: true,
+        destination: docData.url,
+      },
+      props: {},
+    };
+  }
+
+  return {
+    props: {
+      id,
+    },
+  };
+}
