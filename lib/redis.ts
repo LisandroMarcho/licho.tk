61d2d664ab346f69b777a7951c8bd7004e60c2f3
Lisandro Marchionni <djlm2140@gmail.com>
2023-07-16 20:02:43 -0300

    fixed upstash init

--- lib/redis.ts
+++ lib/redis.ts
@@ -2,5 +2,5 @@ import { Redis } from "@upstash/redis";
 
 export const redis = new Redis({
   url: "https://splendid-frog-44057.upstash.io",
-  token: process.env.UPSTASH_TOKEN,
+  token: String(process.env.UPSTASH_TOKEN),
 });
