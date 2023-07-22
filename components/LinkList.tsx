3c3295fe5e90640d6c1e8cd5d4ebf396498b69f5
Lisandro Marchionni <djlm2140@gmail.com>
2023-07-22 18:15:06 -0300

    feat: list skeleton and button spinner

--- components/LinkList.tsx
+++ components/LinkList.tsx
@@ -7,14 +7,25 @@ import { LinkDto } from "../lib/link";
 export default function LinkList() {
   const [baseUrl, setBaseUrl] = useState("");
   const fetcher = (url) => fetch(url).then((res) => res.json());
-  const { data } = useSWR<LinkDto[]>("/api/links", fetcher);
+  const { data, isLoading } = useSWR<LinkDto[]>("/api/links", fetcher);
 
   useEffect(() => {
     setBaseUrl(window.location.origin);
   }, []);
 
   return (
-    <div className="flex flex-col gap-2">
+    <div className={`flex flex-col gap-2 ${isLoading ? "animate-pulse" : ""}`}>
+      {isLoading &&
+        [1, 2, 3].map(() => (
+          <>
+            <div className="bg-secondary border-[8px] p-4">
+              <div className="bg-slate-200 rounded p-2 w-[100px] mb-2"></div>
+              <div className="bg-slate-200 rounded p-3 w-[300px] mb-2"></div>
+              <div className="bg-slate-200 rounded p-2 w-[200px]"></div>
+            </div>
+          </>
+        ))}
+
       {data &&
         data.map((link) => (
           <LinkBox
