eaf54acf49f8cce71330d0f05b477c1f21d6d8c1
Lisandro Marchionni <marchionni.lisandro@gmail.com>
2023-11-26 10:44:37 -0300

    removed links in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,37 +12,6 @@ export default function Footer() {
               />
             </a>
           </div>
-          <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-            <div>
-              <h2 className="mb-6 text-sm font-semibold text-secondary uppercase dark:text-white">
-                Nosotros
-              </h2>
-              <ul className="text-gray-600 dark:text-gray-400">
-                <li className="mb-4">
-                  <a
-                    href="https://github.com/lichodev/licho.tk"
-                    className="hover:underline"
-                  >
-                    Código (Github)
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://lichodev.com.ar/"
-                    className="hover:underline"
-                  >
-                    Acerca de mí
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <hr className="my-6 border-secondary sm:mx-auto lg:my-8" />
-        <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2023 LICHODEV
-          </span>
         </div>
       </div>
     </footer>
