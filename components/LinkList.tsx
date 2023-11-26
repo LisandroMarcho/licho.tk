c89d05eaded10c4100d72ba78cf44b8f41a1e923
Lisandro Marchionni <marchionni.lisandro@gmail.com>
2023-11-26 11:35:31 -0300

    added key to loading boxes

--- components/LinkList.tsx
+++ components/LinkList.tsx
@@ -16,14 +16,12 @@ export default function LinkList() {
   return (
     <div className={`flex flex-col gap-2 ${isLoading ? "animate-pulse" : ""}`}>
       {isLoading &&
-        [1, 2, 3].map(() => (
-          <>
-            <div className="bg-secondary border-[8px] p-4">
-              <div className="bg-slate-200 rounded p-2 w-[100px] mb-2"></div>
-              <div className="bg-slate-200 rounded p-3 w-[300px] mb-2"></div>
-              <div className="bg-slate-200 rounded p-2 w-[200px]"></div>
-            </div>
-          </>
+        [1, 2, 3].map((k) => (
+          <div key={k} className="bg-secondary border-[8px] p-4">
+            <div className="bg-slate-200 rounded p-2 w-[100px] mb-2"></div>
+            <div className="bg-slate-200 rounded p-3 w-[300px] mb-2"></div>
+            <div className="bg-slate-200 rounded p-2 w-[200px]"></div>
+          </div>
         ))}
 
       {data &&
